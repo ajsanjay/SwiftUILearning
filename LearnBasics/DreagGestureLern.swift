cb383560d99bdeaf5e0d31e895213022e69acdb6
Jaya Sabeen <jayasabeen@Jayas-MacBook-Pro.local>
2025-01-16 12:10:17 +0530

    Removed file that shouldn't be tracked

---
+++ LearnBasics/DreagGestureLern.swift
@@ -0,0 +1,18 @@
+//
+//  DreagGestureLern.swift
+//  LearnBasics
+//
+//  Created by Jaya Sabeen on 11/01/25.
+//
+
+import SwiftUI
+
+struct DreagGestureLern: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DreagGestureLern()
+}
