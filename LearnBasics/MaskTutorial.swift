5f5de5eabcaa00df327fae03fbc13cdcff91df8d
Jaya Sabeen <jayasabeen@Jayas-MacBook-Pro.local>
2025-01-28 12:08:57 +0530

    Mask basic

---
+++ LearnBasics/MaskTutorial.swift
@@ -0,0 +1,92 @@
+//
+//  MaskTutorial.swift
+//  LearnBasics
+//
+//  Created by Jaya Sabeen on 28/01/25.
+//
+
+import SwiftUI
+
+struct MaskTutorial: View {
+    
+    @State var rating: Int = 0
+    
+    var body: some View {
+        ZStack {
+            starsView
+                .overlay(OverlayView.mask(starsView))
+        }
+    }
+    
+    private var OverlayView: some View {
+        GeometryReader { geometry in
+            ZStack (alignment: .leading) {
+                Rectangle()
+                    .mask(starsView)
+                    .foregroundColor(.yellow)
+                    .frame(width: CGFloat(rating) / 5 * geometry.size.width)
+            }
+        }
+        .allowsHitTesting(false)
+    }
+    
+    private var starsView: some View {
+        HStack {
+            ForEach (1..<6) { index in
+                Image(systemName: "star.fill")
+                    .font(.largeTitle)
+                    .foregroundColor(.gray)//rating >= index ? .yellow :
+                    .onTapGesture {
+                        withAnimation(.easeOut) {
+                            rating = index
+                        }
+                    }
+            }
+        }
+    }
+}
+
+#Preview {
+    MaskTutorial()
+}
+
+/*
+ struct MaskBootcamp: View {
+     
+     @State var rating: Int = 0
+     
+     var body: some View {
+         ZStack {
+             starsView
+                 .overlay(overlayView.mask(starsView))
+         }
+     }
+     
+     private var overlayView: some View {
+         GeometryReader { geometry in
+             ZStack(alignment: .leading) {
+                 Rectangle()
+                     //.foregroundColor(.yellow)
+                     .fill(LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing))
+                     .frame(width: CGFloat(rating) / 5 * geometry.size.width)
+             }
+         }
+         .allowsHitTesting(false)
+     }
+     
+     private var starsView: some View {
+         HStack {
+             ForEach(1..<6) { index in
+                 Image(systemName: "star.fill")
+                     .font(.largeTitle)
+                     .foregroundColor(Color.gray)
+                     .onTapGesture {
+                         withAnimation(.easeInOut) {
+                             rating = index
+                         }
+                     }
+             }
+         }
+     }
+ }
+ */
